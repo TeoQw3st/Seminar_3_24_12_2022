6f3f0fca6a79ef1129b0c93f17d6db4d9afde894
Pavel <Qw3st@mail.ru>
2022-12-24 11:38:56 +0300

    018

---
+++ Seminar/Example_018/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1 )
+{
+    Console.WriteLine($"X>0 и Y>0");
+}
+else if (num == 2)
+{
+    Console.WriteLine($"X>0 и Y<0");
+}
+else if (num == 3)
+{
+    Console.WriteLine($"X<0 и Y<0");
+}
+else if (num == 4)
+{
+    Console.WriteLine($"X>0 и Y<0");
+}
+else
+{
+    Console.WriteLine($"не существует");
+}

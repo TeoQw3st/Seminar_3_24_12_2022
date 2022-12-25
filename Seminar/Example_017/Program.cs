e05d2766dff4df4a4675740bca7633298123fbfa
Pavel <Qw3st@mail.ru>
2022-12-25 23:52:35 +0300

    ex_017

---
+++ Seminar/Example_017/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Задача №17, опеределяет по координатам в какой четверти находится объет");
+Console.WriteLine("Введите координату Х ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (y>0 && x>0)
+{
+Console.WriteLine($"Точка находится в 1ой четвери");
+}
+else if (y>0 && x<0)
+{
+Console.WriteLine($"Точка находится в 2ой четвери");
+}
+else if (y<0 && x<0)
+{
+Console.WriteLine($"Точка находится в 3ей четвери");
+}
+else if (y<0 && x>0)
+{
+Console.WriteLine($"Точка находится в 4ой четвери");
+}
+else
+{
+    Console.WriteLine($"Неверные кооридинаты");
+}

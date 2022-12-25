25980e665c1139849e50c8090cf6736dd9b1a57d
Pavel <Qw3st@mail.ru>
2022-12-25 15:32:34 +0300

    ex_022

---
+++ Seminar/Example_022/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Эта программа будет выдавать квадраты чисел от 1 до числа N");
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+int scuare = 0;
+while (n<=number) 
+{
+    scuare = Convert.ToInt32(Math.Pow(n, 2));
+    Console.WriteLine($"Квадратные числа от 1 до {number}: квадрат числа {n} равен {scuare};"); 
+
+    n = n + 1; 
+}

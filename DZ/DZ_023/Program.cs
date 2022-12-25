5b099daf3726111fd5d3c7ff3e456e82b7f9b0e1
Pavel <Qw3st@mail.ru>
2022-12-25 15:36:13 +0300

    DZ_023

---
+++ DZ/DZ_023/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Решение задачи №23: Эта программа будет выдавать кубы чисел от 1 до числа N");
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+while (n<=number) 
+{
+    int scuare = Convert.ToInt32(Math.Pow(n, 3));
+    Console.WriteLine($"Кубы числа от 1 до {number}: куб числа {n} равен {scuare};"); 
+
+    n = n + 1; 
+}

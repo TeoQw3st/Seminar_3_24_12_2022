f48bff4fba8dfd1c4972d38cfec23b339b824b6a
Pavel <Qw3st@mail.ru>
2022-12-28 15:32:22 +0300

    dz_019

--- DZ/DZ_019/Program.cs
+++ DZ/DZ_019/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Мы будем пробовать решить задачу №19 через массив");
-Console.WriteLine("Введите пятизначное число");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите любое число");
+long number = Convert.ToInt64(Console.ReadLine());
 string massivenumber = number.ToString();
 Console.WriteLine($"перевели в массив {massivenumber}");
 int size = massivenumber.Length;
@@ -9,8 +9,9 @@ Console.WriteLine($"рамзмер массива {size}");
 int ferstindex = 0;
 int secondindex = size-1;
 int count = 0;
+int count2 = 0;
 while (ferstindex < size/2)
-    {// 
+    {
         if (massivenumber[ferstindex] == massivenumber[secondindex])
         {
             ferstindex = ferstindex +1;
@@ -31,15 +32,18 @@ while (ferstindex < size/2)
         
             ferstindex = ferstindex +1;
             secondindex = secondindex - 1;
+            count2 = count2+1;
         }
             
             
     }
 if (count == size/2)
 {
+    Console.WriteLine($"Количество успешных проверок равно {count}");
     Console.WriteLine($"Введеное число {number} является полидромом");
 }
 else
 {
+    Console.WriteLine($"Количество не успешных проверок равно {count2}");
     Console.WriteLine($"Введеное число {number} не является полидромом");
 }
